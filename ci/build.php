91b0e8e7845ee2dae96776159143b1c2c8cac8fe
Stéphane <rabena.stephane@gmail.com>
2019-07-30 12:05:47 +0200

    refactor(doc): refactor doc generation. This closes #4

--- ci/build.php
+++ ci/build.php
@@ -8,12 +8,53 @@ use Symfony\Component\Finder\Finder;
 $fileSystem = new Filesystem();
 
 $result = [];
+$tmpResult = [];
 
 $filesFinder = new Finder();
 $filesFinder->files()->name('data.json')->in('/app/doc');
 
 foreach ($filesFinder as $file) {
-    $result = json_decode($file->getContents(), true);
+    $tmpResult = json_decode($file->getContents(), true);
+}
+
+$tmpResult = $tmpResult['classes'];
+
+foreach($tmpResult as $command) {
+
+    unset($command['name']);
+    unset($command['shortname']);
+    unset($command['classitems']);
+    unset($command['plugins']);
+    unset($command['extensions']);
+    unset($command['plugin_for']);
+    unset($command['extension_for']);
+    unset($command['file']);
+    unset($command['line']);
+
+    if ($command['arguments']) {
+        $arguments = preg_split("#\\n *#", $command['arguments']);
+        $command['arguments'] = [];
+
+        foreach($arguments as $argument) {
+            $argument = explode(' ', $argument, 2);
+            $command['arguments'][] = [
+                'name' => $argument[0],
+                'description' => $argument[1],
+            ];
+        }
+    }
+
+    if ($command['options']) {
+        $command['options'] = preg_split("#\\n *#", $command['options']);
+
+        // TODO: add name and description like arguments
+    }
+
+    if ($command['examples']) {
+        $command['examples'] = preg_split("#\\n *#", $command['examples']);
+    }
+
+    $result[$command['command']] = $command;
 }
 
 $fileSystem->remove(['/app/doc/commands.js']);
